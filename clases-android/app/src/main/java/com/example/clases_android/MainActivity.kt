357a57a915d2d475223254eeabd6537fdd80f630
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2021-07-20 07:18:59 -0500

    Clase 3 - layouts y ciclo de vida

--- clases-android/app/src/main/java/com/example/clases_android/MainActivity.kt
+++ clases-android/app/src/main/java/com/example/clases_android/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.clases_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonIrCicloVida = findViewById<Button>(
+            R.id.btn_ir_ciclo_vida
+        )
+
+        botonIrCicloVida.setOnClickListener {
+            abrirCicloVida()
+        }
+    }
+
+    fun abrirCicloVida(){
+        val intentExplicito = Intent(
+            this,
+            ACicloVida::class.java
+        )
+
+        // this.startActivity(Intent)
+        startActivity(intentExplicito)
     }
 }
