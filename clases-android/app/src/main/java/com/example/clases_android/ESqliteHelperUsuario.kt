4a337b295a33346d7da418193cf9e31f623afea3
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2021-08-02 09:04:56 -0500

    feat: add class 8

--- clases-android/app/src/main/java/com/example/clases_android/ESqliteHelperUsuario.kt
+++ clases-android/app/src/main/java/com/example/clases_android/ESqliteHelperUsuario.kt
@@ -52,6 +52,64 @@ class ESqliteHelperUsuario(
          scriptConsultarUsuario,
          null
      )
+        val existeUsuario = resultaConsultaLectura.moveToFirst()
+        val usuarioEncontrado = EUsuarioBDD(0, "", "")
+        if(existeUsuario){
+            do {
+                val id = resultaConsultaLectura.getInt(0)
+                val nombre = resultaConsultaLectura.getString(1)
+                val descripcion = resultaConsultaLectura.getString(2)
+                if (id != null) {
+                    // aregloUsuarios.add(
+                    // EUsuarioBDD(id, nombre, descripcion)
+                    // )
+                    usuarioEncontrado.id = id
+                    usuarioEncontrado.nombre = nombre
+                    usuarioEncontrado.descripcion = descripcion
+                }
+            }
+                while(resultaConsultaLectura.moveToNext())
+        }
+        resultaConsultaLectura.close()
+        baseDatosLectura.close()
+        return usuarioEncontrado
+    }
+
+    fun eliminarUsuarioFormulario(id: Int) : Boolean {
+        val conexionEscritura = writableDatabase
+        val resultadoEliminacion = conexionEscritura
+            .delete(
+                "USUARIO", // Tabla
+                "id=?", // Clausula Where
+                arrayOf(
+                    id.toString()
+                ) // Arreglo ordenado de parametro
+            )
+        conexionEscritura.close()
+        return if(resultadoEliminacion.toInt() == -1) false else true
+    }
+
+    fun actualizarUsuarioFormulario(
+        nombre:String,
+        descripcion: String,
+        idActualizar: Int
+    ): Boolean{
+        val conexionEscritura = writableDatabase
+        val valoresAActualizar = ContentValues()
+        valoresAActualizar.put("nombre", nombre)
+        valoresAActualizar.put("descripcion", descripcion)
+
+        val resultadoActualizacion = conexionEscritura
+            .update(
+                "USUARIO",
+                valoresAActualizar,
+                "id=?",
+                arrayOf(
+                    idActualizar.toString()
+                )
+            )
+        conexionEscritura.close()
+        return if (resultadoActualizacion == -1) false else true
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
