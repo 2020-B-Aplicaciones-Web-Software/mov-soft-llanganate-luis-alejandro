ad3b91a2e033d1c33224e62a404d2ed8b54e2089
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2021-09-27 17:58:31 -0500

    feat: add main files and directories for the project

---
+++ Examen2B/app/src/main/java/com/example/examen2b/activities/ActualizarUsuarioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.examen2b.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.examen2b.R
+
+class ActualizarUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actualizar_usuario)
+    }
+}
